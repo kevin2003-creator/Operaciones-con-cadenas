22af4cbde17b2160266f4349d49ed4c2399dc2af
kevin2003-creator <klimaa4@miumg.edu.gt>
2025-07-27 21:02:31 -0600

    first commit

---
+++ src/operaciones_cadenas/Operaciones_cadenas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operaciones_cadenas;
+
+/**
+ *
+ * @author ghost
+ */
+public class Operaciones_cadenas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         VistaPrincipal frm = new VistaPrincipal();
+       frm.show();
+    }
+    
+}
